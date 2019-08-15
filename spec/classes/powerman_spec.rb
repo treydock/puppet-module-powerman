1b33b3631f5b875f894eed5ec86210b24314edde
Trey Dockendorf <tdockendorf@osc.edu>
2019-08-15 16:24:39 -0400

    Rubocop auto correct

--- spec/classes/powerman_spec.rb
+++ spec/classes/powerman_spec.rb
@@ -13,48 +13,39 @@ describe 'powerman' do
       it { is_expected.to contain_class('powerman::params') }
 
       it do
-        is_expected.to contain_package('powerman').only_with({
-          :ensure => 'present',
-          :name   => 'powerman',
-        })
+        is_expected.to contain_package('powerman').only_with(ensure: 'present',
+                                                             name: 'powerman')
       end
 
       it do
-        is_expected.to contain_concat('/etc/powerman/powerman.conf').with({
-          :ensure  => 'present',
-          :owner   => 'root',
-          :group   => 'daemon',
-          :mode    => '0640',
-          :require => 'Package[powerman]'
-        })
+        is_expected.to contain_concat('/etc/powerman/powerman.conf').with(ensure: 'present',
+                                                                          owner: 'root',
+                                                                          group: 'daemon',
+                                                                          mode: '0640',
+                                                                          require: 'Package[powerman]')
       end
 
       it do
-        is_expected.to contain_service('powerman').only_with({
-          :ensure      => 'running',
-          :enable      => 'true',
-          :name        => 'powerman',
-          :hasstatus   => 'true',
-          :hasrestart  => 'true',
-          :subscribe   => 'Concat[/etc/powerman/powerman.conf]',
-        })
+        is_expected.to contain_service('powerman').only_with(ensure: 'running',
+                                                             enable: 'true',
+                                                             name: 'powerman',
+                                                             hasstatus: 'true',
+                                                             hasrestart: 'true',
+                                                             subscribe: 'Concat[/etc/powerman/powerman.conf]')
       end
 
       context 'when server => false' do
-        let(:params) {{ :server => false }}
+        let(:params) { { server: false } }
 
         it { is_expected.to contain_package('powerman') }
         it { is_expected.not_to contain_concat('/etc/powerman/powerman.conf') }
         it do
-          is_expected.to contain_service('powerman').only_with({
-            :ensure  => 'stopped',
-            :enable  => 'false',
-            :name    => 'powerman',
-            :require => 'Package[powerman]',
-          })
+          is_expected.to contain_service('powerman').only_with(ensure: 'stopped',
+                                                               enable: 'false',
+                                                               name: 'powerman',
+                                                               require: 'Package[powerman]')
         end
       end
-
     end # end context
   end # end on_supported_os loop
 end # end describe
